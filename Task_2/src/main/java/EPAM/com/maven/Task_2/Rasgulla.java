3e75cf76e56d23127498c4300d7c0098f3083a7c
Sairajesh221 <sairajesh221@gmail.com>
2020-02-09 00:45:38 +0530

    OOPS AND MAVEN

---
+++ Task_2/src/main/java/EPAM/com/maven/Task_2/Rasgulla.java
@@ -0,0 +1,13 @@
+package EPAM.com.maven.Task_2;
+
+public class Rasgulla  implements sweets{
+
+	
+		public int totalwt(int qnt, int wt)
+		{
+			return qnt*wt;
+		}
+		// TODO Auto-generated constructor stub
+	
+
+}
---
+++ Task_2/src/main/java/EPAM/com/maven/Task_2/rasmalai.java
@@ -0,0 +1,13 @@
+package EPAM.com.maven.Task_2;
+
+public class rasmalai implements sweets{
+
+	
+		public int totalwt(int qnt,int wt)
+		{
+			return wt*qnt;
+		}
+		// TODO Auto-generated constructor stub
+	
+
+}
